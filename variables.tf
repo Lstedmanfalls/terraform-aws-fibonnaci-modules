87ca74b2c637c4b7342ebc5ba06a5ccc2e03b814
Lisa Stedman-Falls <lstedmanfalls@gmail.com>
2024-01-10 16:25:59 -0700

    add constraints to variable inputs

--- variables.tf
+++ variables.tf
@@ -4,22 +4,26 @@ variable "region" {
 }
 
 variable "az" {
-  description = "The regoin's availability zone to deploy these resources into"
+  description = "The region's availability zone to deploy these resources into"
+  nullable    = false
 }
 
 variable "project_name" {
   description = "Application, service, etc. that will use these resources"
   type        = string
+  nullable    = false
 }
 
 variable "environment" {
   description = "Environment to deploy these resources into (development, staging, or production)"
   type        = string
+  nullable    = false
 }
 
 variable "ec2_instance_name" {
   description = "Unique name for this ec2 instance. Environment and project_name will be prefixed automatically to the name"
   type        = string
+  nullable    = false
 }
 
 variable "ec2_instance_type" {
@@ -37,9 +41,13 @@ variable "ec2_monitoring" {
 variable "ec2_sg_ids" {
   description = "List of vpc security group ids to associate with this ec2 instance"
   type        = list(string)
+  nullable    = false
+  sensitive   = true
 }
 
 variable "ec2_subnet_id" {
   description = "Subnet to deploy this ec2 instance into"
   type        = string
+  nullable    = false
+  sensitive   = true
 }

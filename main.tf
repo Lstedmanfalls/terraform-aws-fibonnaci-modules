31a9d5d528da45c800684c9547e91e3fae6e3a38
Lisa Stedman-Falls <lstedmanfalls@gmail.com>
2024-01-10 12:26:58 -0700

    update naming string

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "ec2-instance" {
   source                 = "terraform-aws-modules/ec2-instance/aws"
   version                = "5.6.0"
-  name                   = "${locals.name}-${var.ec2_instance_name}"
+  name                   = "${var.project_name}-${var.environment}-${var.ec2_instance_name}"
   instance_type          = var.ec2_instance_type
   monitoring             = var.ec2_monitoring
   vpc_security_group_ids = var.ec2_sg_ids

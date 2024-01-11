292f1670842732ce0208963157c4ceab1761e6dd
Lisa Stedman-Falls <lstedmanfalls@gmail.com>
2024-01-11 09:13:45 -0700

    add output for azs

--- modules/network/locals.tf
+++ modules/network/locals.tf
@@ -1,4 +1,4 @@
 locals {
   vpc_cidr = "10.0.0.0/16"
-  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+  azs      = slice(data.aws_availability_zones.all.names, 0, 3)
 }
--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -1,3 +1,7 @@
 output "vpc_id" {
   value = module.vpc.default_vpc_id
 }
+
+output "aws_availability_zones" {
+  value = data.aws_availability_zones.all.names
+}

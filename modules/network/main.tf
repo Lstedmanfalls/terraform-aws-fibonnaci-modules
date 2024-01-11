80b2c5ef1d994309e922a934462fb8bfc488075c
Lisa Stedman-Falls <lstedmanfalls@gmail.com>
2024-01-11 13:12:25 -0700

    fix cidr subnet

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -2,5 +2,5 @@ module "vpc" {
   source         = "terraform-aws-modules/vpc/aws"
   name           = "${var.project_name}-${var.environment}"
   azs            = [var.az]
-  public_subnets = [for v in [var.az] : cidrsubnet("10.0.0.0/16", 8, v + 4)]
+  public_subnets = [cidrsubnet("10.0.0.0/16", 4, 2)]
 }

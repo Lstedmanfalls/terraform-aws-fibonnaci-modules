ebecfffbea32e89417a2b33a73acb2cbb587cca6
Lisa Stedman-Falls <lstedmanfalls@gmail.com>
2024-01-16 01:40:00 -0700

    add task definition

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -23,10 +23,40 @@ resource "aws_autoscaling_group" "ecs_autoscaling_group" {
   health_check_grace_period = 300
 }
 
+resource "aws_ecs_task_definition" "task" {
+  family = "${var.project_name}-${var.environment}"
+  container_definitions = jsonencode([
+    {
+      name      = "${var.project_name}-${var.environment}"
+      image     = "${var.project_name}-${var.environment}"
+      cpu       = 10
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 8000
+          hostPort      = 8000
+        }
+      ]
+    },
+  ])
+
+  volume {
+    name      = "${var.project_name}-${var.environment}"
+    host_path = "/ecs/service-storage"
+  }
+
+  placement_constraints {
+    type       = "memberOf"
+    expression = "attribute:ecs.availability-zone in [${var.az}]"
+  }
+}
+
 resource "aws_ecs_service" "app" {
-  name       = "${var.project_name}-${var.environment}"
-  cluster    = aws_ecs_cluster.ecs_cluster.id
-  depends_on = [aws_ecs_cluster.ecs_cluster]
+  name            = "${var.project_name}-${var.environment}"
+  cluster         = aws_ecs_cluster.ecs_cluster.id
+  task_definition = aws_ecs_task_definition.task.arn
+  depends_on      = [aws_ecs_cluster.ecs_cluster]
 
   placement_constraints {
     type       = "memberOf"
